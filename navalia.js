f348a1e82c41dce5feb531a8ac51e5e5edacf01c
Soumyajit Pathak <s.pathak5995@gmail.com>
2017-09-15 21:18:21 +0530

    unstable

--- navalia.js
+++ navalia.js
@@ -1,60 +1,61 @@
 const { Chrome } = require('navalia');
-
 const chrome = new Chrome();
 
-// foreign beers catalog
+const wait = async () => {
+	await chrome.wait(1000);
+	await chrome.evaluate(() => window.showAll());
+	await chrome.wait(1000);
+	await chrome.evaluate(() => window.hidePagination());
+	await chrome.wait(1000);
+};
+
+const setOptions = async (type, cat) => {
+	await chrome.wait('#liqTypeId');
+	await chrome.evaluate(val => {
+		const el = document.querySelector('#liqTypeId');
+		el.value = val;
+		el.onchange();
+	}, type);
+	await chrome.wait(1000);
+	await chrome.evaluate(val => {
+		const el = document.querySelector('#liqCategoryId');
+		el.value = val;
+		el.onchange();
+	}, cat);
+	await chrome.wait(1000);
+};
+
+const getJSON = async () => {
+	return [
+		...document.querySelectorAll('#priceListTable tbody tr:not(:first-child)'),
+	]
+		.map(n =>
+			n.innerHTML
+				.split('>')
+				.filter(x => x.includes('</'))
+				.map(x => x.substr(0, x.length - 4))
+		)
+		.reduce((a, x) => {
+			const beers = [...a];
+			beers.push({
+				slNo: x[0],
+				brandCode: x[1],
+				brandName: x[2],
+				size: x[3],
+				pack: x[4],
+				price: x[5],
+				warehouse: x[6],
+			});
+			return beers;
+		}, []);
+};
 
 (async () => {
 	await chrome.goto('https://delhiexcise.gov.in/Portal/priceList.action');
+	await setOptions('Foreign Liqour', 'Whisky');
+	await wait();
+	const result = await getJSON();
 
-	await chrome.evaluate(() => {
-		const el = document.querySelector('#liqTypeId');
-		el.value = 'Foreign Liquor';
-		el.onchange();
-	});
-
-	await chrome.wait(1000);
-
-	await chrome.evaluate(() => window.showAll());
-
-	await chrome.wait(1000);
-
-	await chrome.evaluate(() => window.hidePagination());
-
-	await chrome.wait(1000);
-
-	const res = await chrome.evaluate(() => {
-		return [
-			...document.querySelectorAll(
-				'#priceListTable tbody tr:not(:first-child)'
-			),
-		]
-			.map(n =>
-				n.innerHTML
-					.split('>')
-					.filter(x => x.includes('</'))
-					.map(x => x.substr(0, x.length - 4))
-			)
-			.reduce(
-				(a, x) => {
-					const beers = [...a.beers];
-					beers.push({
-						slNo: x[0],
-						brandCode: x[1],
-						brandName: x[2],
-						size: x[3],
-						pack: x[4],
-						price: x[5],
-						warehouse: x[6],
-					});
-					a.beers = beers;
-					return a;
-				},
-				{ beers: [] }
-			);
-	});
-
-	console.log(res);
-
+	console.log(result);
 	chrome.done();
 })();
